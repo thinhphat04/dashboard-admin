0a68686a5374df1be2f2bc5c31ea9143474c11b7
BuiTuanKhai15052001 <buituankhai2001@gmail.com>
2024-01-13 11:18:52 +0700

    add file project

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+const {
+  AdminRoute,
+  UserRoute,
+  ProductRoute,
+  PromoteRoute,
+  FeedbackRoute,
+  OrderRoute,
+  GiftcodeRoute,
+  CommentRoute,
+} = require('./routes');
+
+const app = express();
+const http = require('http').Server(app);
+const PORT = process.env.PORT || 4000;
+
+dotenv.config();
+
+connectDB();
+
+app.use(express.json({ limit: '4mb' }));
+
+app.use(
+  morgan('dev', {
+    skip: function (req) {
+      if (req.url.indexOf('socket') >= 0) {
+        return true;
+      }
+      return false;
+    },
+  }),
+);
+app.use(cors());
+app.use('/public', express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>ShopTECH E-commerce Server</h1>');
+});
+
+app.use('/api/admins', AdminRoute);
+app.use('/api/users', UserRoute);
+app.use('/api/products', ProductRoute);
+app.use('/api/promotes', PromoteRoute);
+app.use('/api/feedbacks', FeedbackRoute);
+app.use('/api/orders', OrderRoute);
+app.use('/api/giftcodes', GiftcodeRoute);
+app.use('/api/comments', CommentRoute);
+
+http.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`Conneted To Mongodb Databse ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error in Mongodb ${error}`);
+  }
+};
+
+module.exports = connectDB;
